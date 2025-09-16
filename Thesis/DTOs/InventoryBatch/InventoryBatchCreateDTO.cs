90d12897b4648a28c05636f8ae35a19540d8782f
JULIUS ELEAZAR PINEDA LAJOM <julius.lajom.college@ustangelicum.edu.ph>
2025-09-16 20:55:52 +0800

    ALL DTOs Done

---
+++ Thesis/DTOs/InventoryBatch/InventoryBatchCreateDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace Thesis.DTOs.InventoryBatch
+{
+    public class InventoryBatchCreateDTO
+    {
+        public int ItemId { get; set; }
+
+        public string BatchNumber { get; set; }
+
+        public int QuantityInStock { get; set; }
+
+        public DateOnly ExpirationDate { get; set; }
+
+        public DateOnly DateReceived { get; set; }
+    }
+}
---
+++ Thesis/DTOs/InventoryBatch/InventoryBatchUpdateDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace Thesis.DTOs.InventoryBatch
+{
+    public class InventoryBatchUpdateDTO
+    {
+        public string BatchNumber { get; set; }
+
+        public int QuantityInStock { get; set; }
+
+        public DateOnly ExpirationDate { get; set; }
+
+        public DateOnly DateReceived { get; set; }
+    }
+}
